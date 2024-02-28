e1609fc7d61bb534606eb0ba526df67e769c2cce
Somonor Hong <150505791+somonor011@users.noreply.github.com>
2024-02-28 20:12:50 +0700

    Add files via upload

---
+++ Array/change-value-array/app.js
@@ -0,0 +1,5 @@
+// change value array
+
+const array = ["A","B","C"];
+array[1] = "Z"; // array change B tp Z
+console.log(array);
---
+++ Array/create-show-item/app.js
@@ -0,0 +1,5 @@
+// create array
+
+const Information =["Hong Somonor","CS",2006];
+
+console.log(Information);
